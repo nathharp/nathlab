92df5fbcd818802bd772b537e77f247dde942e1e
Nathan Harper <nathan.harper@cfms.org.uk>
2015-03-31 16:18:49 +0100

    added menu option example

--- nathlab-vbox/vboxcreate.sh
+++ nathlab-vbox/vboxcreate.sh
@@ -2,8 +2,33 @@
 
 # the objective of this script is to automate the creation of VMs under virtualbox.  Although this could be done under Vagrant, I want to have a bit more control over what is being done!
 
+
+
 # example prompt with default
 default=192.168.0.4
 read -p "Enter IP address [$default]: " REPLY
 REPLY=${REPLY:-$default}
 echo "IP is $REPLY"
+
+# Bash Menu Script Example
+
+PS3='Please enter your choice: '
+options=("Option 1" "Option 2" "Option 3" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            echo "you chose choice 1"
+            ;;
+        "Option 2")
+            echo "you chose choice 2"
+            ;;
+        "Option 3")
+            echo "you chose choice 3"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
