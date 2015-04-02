4fc7ec3bc00ace1d4b9b5f74442b4aea0bbbbf9f
Nathan Harper <nathan.harper@cfms.org.uk>
2015-04-02 16:37:01 +0100

    initial version
    no modifiable config, apart from hostname

---
+++ nathlab-vbox/cloud-init-config.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# do the metadata
+
+
+
+mkdir -p ~/tmp/$1-tmpconfig
+cd  ~/tmp/$1-tmpconfig
+echo "local-hostname: $1" > meta-data
+
+# do some user data
+
+cat << EOF > user-data
+cloud-config
+password: passw0rd
+chpasswd: { expire: False }
+ssh_pwauth: True
+ssh_authorized_keys:
+ - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxuUBmKq0gQWnDtIg7f3S4cYvqJn3GD4Pf9Lx4I5m6L4wOCgYndS5dyFRaMl0HO2dtd3seN9U43kgO8+Ajev7PGKwMWVqgOk2i2hAlbBQRPgV8lx5wLHXf1ML7XvNf7eEYD9kBgkkaq/FzyfmrUiN454bbLUlfn8SHTT+H6d2Ux3M5AJtcndBe23gjHK8So17JWT4KPBbG3VTJIZ917zx6QxY0Wbkwdcut6zzN69ihYxm0dso6JZQa+ZeaFm6YgJOkS5INanIjdTpzKPe7Z339vGFGswCJDSL4x/MGG3+Z3JwbbUToWGWd8n+EK11gEkjC7QgB17GC0lEGHCokn7Hr nathharp@Nathans-MacBook-Pro.local
+bootcmd:
+ - echo 10.20.0.4 labmgt >> /etc/hosts
+
+yum_repos:
+    local-lab-centos-otherpkgs:
+        baseurl: http://labmgt/repo/centos7/bootstrap/
+        enabled: true
+        gpgcheck: false
+        name: lab centos7 bootstrap
+
+packages:
+ - salt-minion
+
+salt_minion:
+  conf:
+    master: labmgt
+EOF
