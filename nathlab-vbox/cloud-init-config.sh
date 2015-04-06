d5c903c46297e021a3ebf5a11617fd1d42a73908
Nathan Harper <nathan.harper@cfms.org.uk>
2015-04-06 17:40:00 +0100

    added lines to configure and bring up enp0s17

--- nathlab-vbox/cloud-init-config.sh
+++ nathlab-vbox/cloud-init-config.sh
@@ -21,6 +21,8 @@ ssh_authorized_keys:
  - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxuUBmKq0gQWnDtIg7f3S4cYvqJn3GD4Pf9Lx4I5m6L4wOCgYndS5dyFRaMl0HO2dtd3seN9U43kgO8+Ajev7PGKwMWVqgOk2i2hAlbBQRPgV8lx5wLHXf1ML7XvNf7eEYD9kBgkkaq/FzyfmrUiN454bbLUlfn8SHTT+H6d2Ux3M5AJtcndBe23gjHK8So17JWT4KPBbG3VTJIZ917zx6QxY0Wbkwdcut6zzN69ihYxm0dso6JZQa+ZeaFm6YgJOkS5INanIjdTpzKPe7Z339vGFGswCJDSL4x/MGG3+Z3JwbbUToWGWd8n+EK11gEkjC7QgB17GC0lEGHCokn7Hr nathharp@Nathans-MacBook-Pro.local
 bootcmd:
  - echo 10.20.0.4 labmgt >> /etc/hosts
+ - printf "DEVICE=enp0s17\nONBOOT=yes\nBOOTPROTO=dhcp\n" > /etc/sysconfig/network-scripts/ifcfg-enp0s17
+ - ifup enp0s17
  - rm -f /etc/yum.repos.d/CentOS*.repo
  
 
